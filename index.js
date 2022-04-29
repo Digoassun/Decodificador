172a3e456c3c2d41cd46a56da3bae88eb24d5b6d
Digoassun <digoassun@gmail.com>
2022-04-29 00:38:43 -0300

    Primeiro commit com html e css em andamento

---
+++ index.js
@@ -0,0 +1,7 @@
+var passos = document.querySelector('#passos')
+var selection = document.querySelector('#selection')
+var cifraOption = document.querySelector('#cifraOption')
+
+cifraOption.onclick = function(){
+    passos.style.display = 'flex'
+}
