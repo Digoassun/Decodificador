30f6ee469d034c7e884283f56dc647c2e33ec97e
Digoassun <digoassun@gmail.com>
2022-04-29 23:02:10 -0300

    Alterações de estilo e funcionamento de js com seleção de inputs

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 var passos = document.querySelector('#passos')
-var selection = document.querySelector('#selection')
-var cifraOption = document.querySelector('#cifraOption')
+var select = document.querySelector('#select')
+var radio = document.querySelector('#radio')
+var codifier = document.querySelector('#codifier')
+var decodifier = document.querySelector('#decodifier')
+var btn = document.querySelector('#btn')
 
-cifraOption.onclick = function(){
-    passos.style.display = 'flex'
-}
+select.addEventListener('click', function(event){
+    if(select.value =='3'){
+        passos.style.display = 'flex'
+    }else{
+        passos.style.display = 'none'
+    }
+})
+
+radio.addEventListener('click', function(event){
+    if(codifier.checked){
+        btn.innerHTML = `<input class="btnInput" type="button" value="Codificar mensagem">`
+    }
+})
+
+radio.addEventListener('click', function(event){
+    if(decodifier.checked){
+        btn.innerHTML = `<input class="btnInput" type="button" value="Decodificar mensagem">`
+    }
+})
+
