fd8d4ed0d39864e9f2bf32e3100ef87cf9be3fda
Digoassun <digoassun@gmail.com>
2022-05-01 20:02:11 -0300

    codificador e decodificador do Base64 funcional

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
 var passos = document.querySelector('#passos')
 var select = document.querySelector('#select')
 var radio = document.querySelector('#radio')
-var codifier = document.querySelector('#codifier')
-var decodifier = document.querySelector('#decodifier')
+var encode = document.querySelector('#encode')
+var decode = document.querySelector('#decode')
 var btn = document.querySelector('#btn')
 
-select.addEventListener('click', function(event){
-    if(select.value =='3'){
+//Função para aparecer os passos
+select.addEventListener('click', function (event) {
+    if (select.value == '3') {
         passos.style.display = 'flex'
-    }else{
+    } else {
         passos.style.display = 'none'
     }
 })
 
-radio.addEventListener('click', function(event){
-    if(codifier.checked){
-        btn.innerHTML = `<input class="btnInput" type="button" value="Codificar mensagem">`
+
+//Funções para butões
+radio.addEventListener('click', function (event) {
+    if (encode.checked) {
+        btn.innerHTML = `<input class="btnInput" id="btnEncoder" type="button" value="Codificar mensagem" onclick="encoder()">`
+    } else if (decode.checked) {
+        btn.innerHTML = `<input class="btnInput" id="btnDecoder" type="button" value="Decodificar mensagem" onclick="decoder()">`
     }
 })
 
-radio.addEventListener('click', function(event){
-    if(decodifier.checked){
-        btn.innerHTML = `<input class="btnInput" type="button" value="Decodificar mensagem">`
-    }
-})
+//Base64 e Cesar ENCODE
+// var btnEncoder = document.querySelector('#btnEncoder')
+// var btnDecoder = document.querySelector('#btnDecoder')
+var enterTxt = document.querySelector('#txt')
+var txtValue;
+var outTxt = document.querySelector('#finalTxt')
 
+function encoder() {
+    if (select.value == '2' && encode.checked) {
+        txtValue = enterTxt.value
+        outTxt.value = btoa(txtValue)
+    }
+}
+
+//Base64 e Cesar DECODE
+
+function decoder() {
+    if (select.value == '2' && decode.checked) {
+        txtValue = enterTxt.value
+        outTxt.value = atob(txtValue)
+    }
+}
